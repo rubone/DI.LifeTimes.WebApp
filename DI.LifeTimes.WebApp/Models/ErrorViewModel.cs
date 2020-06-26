92913a0ae9b8da1f5469af8f47b835048d0aae0f
Ruben Saavedra <ruben.saavedra@ahinko.com>
2020-06-25 22:19:46 -0600

    Add project files.

---
+++ DI.LifeTimes.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DI.LifeTimes.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
